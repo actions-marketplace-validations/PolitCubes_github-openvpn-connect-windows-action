9e9832b3ddf9812672b62752323fcf99a88d4812
Clercqer <pashok2102@yahoo.com>
2023-08-20 07:24:36 +0300

    bugfix

--- src/main.js
+++ src/main.js
@@ -53,7 +53,7 @@ const run = (callback) => {
   const tail = new Tail("openvpn.log");
 
   try {
-    exec(`"C:/Program Files/OpenVPN/bin/openvpn.exe" --config ${configFile} --log openvpn.log --writepid openvpn.pid`);
+    exec(`"C:/Program Files/OpenVPN/bin/openvpn.exe" --config ${configFile}`);
   } catch (error) {
     core.error(fs.readFileSync("openvpn.log", "utf8"));
     tail.unwatch();
